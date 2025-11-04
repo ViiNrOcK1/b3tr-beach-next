0dbc12eebe7e776e2679e4741267e599c012e0fb
ViiNrOcK1 <vincent.delgado@gmail.com>
2025-11-03 21:33:06 -0500

    Add root route: / → Homepage

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import Homepage from '@/components/Homepage';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>B3TR BEACH Homepage</title>
+      </Head>
+      <Homepage />
+    </>
+  );
+}
