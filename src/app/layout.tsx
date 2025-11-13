3cf1d92e1a78f8c50f84a9b97454c13ab93f26b0
ViiNrOcK1 <vincent.delgado@gmail.com>
2025-11-12 22:28:34 -0500

    fix: remove MascotsWrapper, move mascots + 5s delay into layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,17 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import DAppKitProviderWrapper from './DAppKitProviderWrapper';
 import { APP_DESCRIPTION } from './config';
-import MascotsWrapper from '@/components/MascotsWrapper';
+
+// 1. Import the mascot component and all the data
+import DraggableMascot from '@/components/DraggableMascot';
+import { 
+  inkyFacts, 
+  rangerFacts, 
+  inkyIdleGif, 
+  inkyFactGifs, 
+  rangerIdleGif, 
+  rangerFactGifs 
+} from '@/lib/mascots';
 
 const geistSans = Geist({ variable: '--font-geist-sans', subsets: ['latin'] });
 const geistMono = Geist_Mono({ variable: '--font-geist-mono', subsets: ['latin'] });
@@ -19,7 +29,27 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <DAppKitProviderWrapper>
           {children}
-          <MascotsWrapper />
+
+          {/* 2. Add the mascots directly here. 
+            We removed the broken MascotsWrapper. 
+          */}
+          <DraggableMascot
+            idleImageSrc={inkyIdleGif}
+            factImageSrcs={inkyFactGifs}
+            altText="Inky the Octopus"
+            facts={inkyFacts}
+            initialPosition={{ x: 30, y: 200 }}
+            animationType="jump"
+          />
+          <DraggableMascot
+            idleImageSrc={rangerIdleGif}
+            factImageSrcs={rangerFactGifs}
+            altText="Ranger Bear"
+            facts={rangerFacts}
+            initialPosition={{ x: 150, y: 350 }}
+            animationType="slide"
+          />
+
         </DAppKitProviderWrapper>
       </body>
     </html>
